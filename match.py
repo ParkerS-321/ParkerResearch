c0351ef684c142c453c40afba12aafa2c69050df
ParkerS321 <ps18@my.fsu.edu>
2022-01-15 15:04:36 -0500

    new commit u301

--- match.py
+++ match.py
@@ -3,7 +3,7 @@ import re
 import os
 import jinja2
 import pandas as pd
-from IPython.display import HTML
+from IPython.display import Image,HTML
 import numpy as np
 
 list_files=glob.glob("**", recursive=True) 
@@ -11,42 +11,42 @@ list_files=glob.glob("**", recursive=True)
 new_list=[]
 
 for file in list_files:
-	head,tail = os.path.split(file)
-	new_list.append(tail)
-	
+	#head,tail = os.path.split(file)
+	#new_list.append(tail)
+	new_list.append(file)
 #print(new_list)
 
-regex = re.compile('u301_density_6_c(\d{4}).png')
+regex = re.compile('(.*)/u301_density_6_c(\d{4}).png')
 
 print("The files matching this format are: ")
 
-final_list=[]
+image_list=[]
 core_list = []
 
 for file in new_list:
 	match = regex.match(file)
 	if match:
-		final_list.append(file)	
+		image_list.append(file)	
 		found = match
 		print(file)
-		#core_num = match.groups()
-	 	#core_list.append(core_num)
-		#print(core_list)
 
-for file in final_list:
+for file in image_list:
 	match = regex.match(file)
 	if match:
-		core_num = match.groups()
-		core_list.append(core_num)
+		core_num = match.group(2)
+		core_list.append(core_num)		
+
+print(core_list)
 
 
 
+df = pd.DataFrame(list(zip(core_list,image_list)), columns = ['Core ID', 'Image'])
 
-df = pd.DataFrame(core_list,columns=['Core ID'])
-df['Image'] = final_list
+#df = pd.DataFrame(core_list,columns=['Core ID'])
+#df['Image'] = image_list
 
 def path_to_image_html(path):
-	return '<img src="'+ path + '" width= "60">'
+	return '<img src="' + path + '" width= "120">'
 
 pd.set_option('display.max_colwidth', None)
 
@@ -59,12 +59,6 @@ for image_col in image_cols:
 
 df.to_html('u301_table.html',escape=False, formatters=format_dict)
 
-
-#df = pd.DataFrame(list(zip(core_list,final_list)),
-#	columns=['Core ID', 'Image'])
-
-#result = df.to_html('u301_table.html')
-#print(result)
 
 
 #------------------------------------------------------------------
