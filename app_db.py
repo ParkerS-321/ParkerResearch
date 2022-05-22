e75f9ee462035c408810363852a5cd9d3462a354
ParkerS321 <ps18@my.fsu.edu>
2022-05-22 13:46:34 -0400

    Multiple Cores and Products

--- app_db.py
+++ app_db.py
@@ -33,10 +33,18 @@ def getCore():
 	core_list = cores_id.split(",")
 	core_tuple = tuple(core_list)
 	
-#	products = request.form.get('products')
-#	product_list = products.split(",")
-#	prod_tuple = tuple(product_list)
+	product_list = request.form.getlist('mycheckbox')
+#	product_list = product_list.split(",")
+	prod_tuple = tuple(product_list)
 
+
+	blowhair_tuple = []
+	densitytime_tuple = []
+
+	table_list = list(db.execute('SELECT name from sqlite_master where type="table";'))
+	y = {}
+	
+	core_query_list = []
 
 	if value == '1':
 		#query = "SELECT blowhair.sim1_img FROM blowhair WHERE blowhair.core IN ({core_list}) UNION SELECT densitytime.sim1_img FROM densitytime WHERE densitytime.core IN ({core_list})"
@@ -44,27 +52,33 @@ def getCore():
 		db.execute(query, core_list)
 
 	if value == '2':
-		query = "SELECT * FROM blowhair WHERE core IN ({co}) ORDER BY core ASC".format(co=','.join(['?'] * len(core_list)))
-		db.execute(query, core_list)
-		#db.execute('SELECT blowhair.sim2_img, densitytime.sim2_img FROM blowhair,densitytime WHERE blowhair.core=? AND densitytime.core=?',(core_id, core_id,))
+		for tablename in table_list:
+			y[tablename] = []
+			if tablename[0] in product_list:
+				product = str(tablename[0])
+				query = "SELECT {pro}.core,{pro}.sim2_img FROM {pro} WHERE core IN ({co}) ORDER BY core ASC".format(co=','.join(['?'] * len(core_list)), pro=product)
+				db.execute(query, core_list)
+				rows = db.fetchall()
+				for row in rows:
+					y[tablename].append(b64encode(row[1]).decode('utf-8'))
+					core_query_list.append(row[0])
 
 	if value == '3':
 		db.execute('SELECT blowhair.sim3_img, densitytime.sim3_img FROM blowhair,densitytime WHERE blowhair.core=? AND densitytime.core=?',(core_id, core_id,))
 
-	rows=db.fetchall()
 
-	img_list = []
-	decode_img_list = []
-	user_core_list = []
-	#n = 2
-	#img_list.append([x[n] for x in rows])
 	
-	for row in rows:
-		decode_img_list.append(b64encode(row[2]).decode('utf-8'))	
-		user_core_list.append(row[1])	
-	blowhair_tup = tuple(zip(user_core_list,decode_img_list))
 	
-	return render_template('table.html',core_list=core_list, blowhair_tup=blowhair_tup)
+	#for value in y.values():
+	#	decode_dt_list.append(b64encode(row[2]).decode('utf-8'))	
+	#	user_core_list.append(row[1])	
+	#blowhair_tup = tuple(zip(user_core_list,decode_img_list))
+	
+
+	list0 = list(y.values())[0]
+	list1 = list(y.values())[1]
+	
+	return render_template('table.html',core_query_list=core_query_list, list0=list0,list1=list1)
 
 if __name__ == "__main__":
 	app.run(debug=True)
